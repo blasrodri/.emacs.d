7b98b56ce1ea131d90e67f8bc1a24e85d97b52fa
Blas Rodriguez Irizar <bir@omnio.net>
2020-02-12 21:27:51 +0100

    add hooks file

---
+++ hooks.el
@@ -0,0 +1,17 @@
+(require 'python-mode)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (elpy-mode)
+	    ))
+
+
+
+(require 'rust-mode)
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+(add-hook 'rust-mode-hook
+	  (lambda ()
+	    (cargo-minor-mode)
+	    ))
