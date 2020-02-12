e86d3cdd55cedd79b155c08228edfd8d312d71ed
Blas Rodriguez Irizar <bir@omnio.net>
2020-02-12 14:17:34 +0100

    Create configuration from scratch

---
+++ init.el
@@ -0,0 +1,35 @@
+; list the packages you want
+(setq package-list '(evil gruvbox-theme helm-rg magit rust-mode))
+
+; list the repositories containing them
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; Theme
+(load-theme 'gruvbox t)
+
+;; Evil mode
+(require 'evil)
+(evil-mode 1)
+
+;; Custom keybindings
+(load "~/.emacs.d/kbd.el")
+
+;; Number mode
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode t)
+
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
---
+++ kbd.el
@@ -0,0 +1,3 @@
+
+;; Helm rg
+(global-set-key (kbd "C-x C-a g")   'helm-rg)
